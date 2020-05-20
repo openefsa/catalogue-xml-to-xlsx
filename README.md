bee7c6d541313d6e49c37558c389cb0137d13c87
Alban.Shahaj <Alban.Shahaj@nbsimbol242.ad.simbologica1.it>
2020-05-20 16:15:11 +0200

    changes to the readme file

--- README.md
+++ README.md
@@ -5,29 +5,16 @@
 # Catalogue xml to xlsx
 This maven project module, written in Java, can be used for converting EFSA's catalogues from xml to xlsx.
 
-# Dependencies
+## Dependencies
 All project dependencies are listed in the [pom.xml](pom.xml) file.
 
 ## Import the project
 In order to import the project correctly into the integrated development environment (e.g. Eclipse), it is necessary to download the project together with all its dependencies.
 The project and all its dependencies are based on the concept of "project object model" and hence Apache Maven is used for the specific purpose.
-In order to correctly import the project into the IDE it is firstly required to create a parent POM Maven project (check the following [link](https://maven.apache.org/guides/introduction/introduction-to-the-pom.html) for further information). 
-Once the parent project has been created add the project and all the dependencies as "modules" into the pom.xml file as shown below: 
-
-	<modules>
-
-		<!-- dependency modules -->
-		<module>module_1</module>
-		...
-		...
-		...
-		<module>module_n</module>
-		
-	</modules>
+In order to correctly import the project into the IDE it is firstly required to clone the project locally through Git and hence import the project in the IDE.
+Once the project has been imported in Eclipse add the required projects and all the dependencies to the build path.
+Find all the requested dependencies in the pom.xml file.
 	
-Next, close the IDE and extract all the zip packets inside the parent project.
-At this stage you can simply open the IDE and import back the parent project which will automatically import also the project and all its dependencies.
 
-_Please note that the "SWT.jar" and the "Jface.jar" libraries (if used) must be downloaded and installed manually in the Maven local repository since are custom versions used in the tool ((install 3rd party jars)[https://maven.apache.org/guides/mini/guide-3rd-party-jars-local.html]). 
-Download the exact version by checking the Catalogue browser pom.xml file._
+_Please note that the "SWT.jar" and the "Jface.jar" libraries (if used) must be downloaded and installed manually in the Maven local repository since are custom versions used in the tool ((install 3rd party jars)[https://maven.apache.org/guides/mini/guide-3rd-party-jars-local.html])._
 
--- src/main/java/xml_to_excel/XsltCompiler.java
+++ src/main/java/xml_to_excel/XsltCompiler.java
@@ -14,50 +14,51 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class XsltCompiler {
 
 	private static final Logger LOGGER = LogManager.getLogger(XsltCompiler.class);
-	
+
 	private String inputFilename;
 	private String xsltFilename;
 	private String outputFilename;
-	
+
 	/**
-	 * Apply a XSLT transformation to the XLM input file. The created XML output filename is
-	 * specified with the outputFilename string.
+	 * Apply a XSLT transformation to the XLM input file. The created XML output
+	 * filename is specified with the outputFilename string.
+	 * 
 	 * @param inputFilename
 	 * @param xsltFilename
 	 * @param outputFilename
 	 * @throws TransformerException
 	 */
-	public XsltCompiler( String inputFilename, String xsltFilename, String outputFilename ) {
+	public XsltCompiler(String inputFilename, String xsltFilename, String outputFilename) {
 
 		this.inputFilename = inputFilename;
 		this.xsltFilename = xsltFilename;
 		this.outputFilename = outputFilename;
 	}
-	
+
 	/**
 	 * Transform the input xml file using the xslt file
-	 * @throws TransformerException 
+	 * 
+	 * @throws TransformerException
 	 */
-	public void compile () throws TransformerException {
-		
+	public void compile() throws TransformerException {
+
 		// prepare xslt source file
-		InputStream stream = this.getClass().getClassLoader().getResourceAsStream( xsltFilename );
-		Source xslt = new StreamSource( stream );
+		InputStream stream = XsltCompiler.class.getClassLoader().getResourceAsStream(xsltFilename);
+		Source xslt = new StreamSource(stream);
 
 		// Get a factory instance to apply the xslt
 		TransformerFactory factory = TransformerFactory.newInstance();
-		
+
 		// set the transformer
-		Transformer transformer = factory.newTransformer( xslt );
+		Transformer transformer = factory.newTransformer(xslt);
 
 		// transform the input with the xslt and create the output
-		StreamSource text = new StreamSource( new File( inputFilename ) );
-		StreamResult output = new StreamResult( new File( outputFilename ) );
-		transformer.transform( text, output );
+		StreamSource text = new StreamSource(new File(inputFilename));
+		StreamResult output = new StreamResult(new File(outputFilename));
+		transformer.transform(text, output);
 
 		try {
 			transformer.reset();
